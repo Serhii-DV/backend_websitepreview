04e165dc147d30c5c99852d27191d98b89e6aa79
Sergey Dyagovchenko <sergey.dyagovchenko@gmail.com>
2012-09-07 19:42:16 +0300

    prepare for Contao 3.x compability

--- system/modules/backend_websitepreview/config/config.php
+++ system/modules/backend_websitepreview/config/config.php
@@ -1,31 +1,14 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
+ *
  * Copyright (C) 2005-2012 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Sergey Dyagovchenko (DyaGa), 2010-2012
- * @author     Sergey Dyagovchenko (DyaGa), http://d.sumy.ua/
- * @package    WebsitePreview
- * @license    LGPL
- * @filesource
+ * @package   backend_websitepreview
+ * @author    Sergey Dyagovchenko (DyaGa), http://d.sumy.ua
+ * @license   LGPL
+ * @copyright Sergey Dyagovchenko 2010-2012
  */
 
 if (TL_MODE == 'BE')
@@ -34,8 +17,6 @@ if (TL_MODE == 'BE')
 
 	$GLOBALS['TL_JAVASCRIPT'][] = '/system/modules/backend_websitepreview/html/backend_websitepreview.js';
 	$GLOBALS['TL_MOOTOOLS'][] = '<script>var WSPData = {websiteTitle: "'. $GLOBALS['TL_CONFIG']['websiteTitle'] .'", useFavicon: '. (int)$useFavicon .'};</script>';
-	
+
 	unset($useFavicon);
 }
-
-?>
--- system/modules/backend_websitepreview/languages/en/modules.php
+++ system/modules/backend_websitepreview/languages/en/modules.php
@@ -1,33 +1,18 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
+ *
  * Copyright (C) 2005-2012 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Sergey Dyagovchenko [DyaGa], 2010-2012
- * @author     Sergey Dyagovchenko [DyaGa], http://d.sumy.ua/
- * @package    WebsitePreview
- * @license    LGPL
- * @filesource
+ * @package   backend_websitepreview
+ * @author    Sergey Dyagovchenko &#40;DyaGa&#41;, http://d.sumy.ua
+ * @license   LGPL
+ * @copyright Sergey Dyagovchenko 2010-2012
  */
 
-$GLOBALS['TL_LANG']['MOD']['backend_websitepreview'] = array('Backend Website Preview', 'Adds a preview link for website in backend header. The link are opens a website in a new window without starndart Contao site preview module.');
 
-?>
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['backend_websitepreview'] = array('Backend Website Preview', 'Adds a preview link for website in backend header. The link are opens a website in a new window without starndart Contao site preview module.');
