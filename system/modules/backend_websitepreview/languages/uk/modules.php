7c6cafae8c6ec08cbe054fa475177b83413bb0e1
Sergey Dyagovchenko <sergey.dyagovchenko@gmail.com>
2013-04-10 12:22:20 +0300

    Add ukrainian language file

---
+++ system/modules/backend_websitepreview/languages/uk/modules.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * @package   backend_websitepreview
+ * @author    Sergey Dyagovchenko (DyaGa), http://d.sumy.ua
+ * @license   LGPL
+ * @copyright Sergey Dyagovchenko 2010-2013
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['backend_websitepreview'] = array('Перегляд сайту у нововму вікні', 'Додає посилання перегляду сайту до заголовку адміністративної частини. Посилання дозволяє відкривати сайт у нововму вікні без використовування стандартного модуля перегляду сайта.');
